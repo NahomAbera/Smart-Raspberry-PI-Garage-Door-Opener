e162fa9674c577c408d6f1040c69dfaee87df877
Nahom Tesfahun Abera <144270821+NahomAbera@users.noreply.github.com>
2024-05-14 18:57:37 -0400

    Version --4

--- main/main.py
+++ main/main.py
@@ -55,7 +55,7 @@ def excute_command():
                     GPIO.output(pin, GPIO.LOW)  
                     time.sleep(1)  
                     GPIO.output(pin, GPIO.HIGH)
-                    time.sleep(1)
+                    time.sleep(0.5)
                     db.collection('Commands').document(door_id).set({'last_command': current_command}, merge=True)
     except Exception as e:
         print("Error in excute_command:", e)
